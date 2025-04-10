c4e0b8fca0fcbbb5fbbf9ca29774d3fbc2bdf7ea
wwdd <qq1060666498@163.com>
2025-04-10 13:49:37 +0800

    add some surprise emement

--- reaction.py
+++ reaction.py
@@ -1,19 +1,20 @@
-# reaction_step2.py
+# reaction_step3.py
 from gpiozero import LED
 from time import sleep
+from random import uniform
 
-# Initialize LED on GPIO4
 led = LED(4)
 
 try:
-    print("Testing LED control...")
+    print("Random interval test")
     while True:
+        delay = uniform(5, 10)
         led.on()
-        print("LED ON")
-        sleep(2)
+        print(f"LED ON for {delay:.2f} seconds")
+        sleep(delay)
         led.off()
         print("LED OFF")
-        sleep(2)
+        sleep(1)  # Brief pause between cycles
 except KeyboardInterrupt:
     led.off()
     print("\nTest terminated")
